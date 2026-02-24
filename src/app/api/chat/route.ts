3c80d615e83fcf81edcd9ea9e51548d888d1777d
eevan7a9 <eevan7a9@gmail.com>
2026-02-24 23:00:36 +0800

    perf: allow gemini key support

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,9 +2,14 @@ import OpenAI from "openai";
 import { NextResponse } from "next/server";
 import type { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPEN_API_TOKEN!,
-});
+const openai = process.env.OPEN_API_TOKEN
+  ? new OpenAI({
+      apiKey: process.env.OPEN_API_TOKEN!,
+    })
+  : new OpenAI({
+      apiKey: process.env.GEMINI_API_KEY!, // Gemini key
+      baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+    });
 
 export async function POST(req: Request) {
   const { messages, context } = await req.json();
@@ -22,7 +27,8 @@ export async function POST(req: Request) {
   ];
 
   const completion = await openai.chat.completions.create({
-    model: "gpt-4.1", // "gpt-3.5-turbo",
+    // model: "gpt-4.1", // "gpt-3.5-turbo",
+    model: "gemini-2.5-flash",
     messages: formattedMessages,
   });
 
